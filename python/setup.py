a25a0af2dcc8677f0399c4d8079dd754c04390c8
Pyo <pyo@robotis.com>
2026-01-15 12:33:21 +0900

    Rebase from 'upstream'

--- python/setup.py
+++ python/setup.py
@@ -6,6 +6,7 @@ import platform
 authors_info = [
     ('Leon Jung', 'rwjung@robotis.com'),
     ('Wonho Yun', 'ywh@robotis.com'),
+    ('Hyungyu Kim', 'kimhg@robotis.com'),
 ]
 
 authors = ', '.join(author for author, _ in authors_info)
@@ -13,11 +14,11 @@ author_emails = ', '.join(email for _, email in authors_info)
 
 setup(
     name='dynamixel_sdk',
-    version='3.8.4',
+    version='4.0.3',
     packages=['dynamixel_sdk'],
     package_dir={'': 'src'},
     license='Apache 2.0',
-    description='Dynamixel SDK 3. python package',
+    description='Dynamixel SDK 4. python package',
     long_description=open('README.txt').read(),
     url='https://github.com/ROBOTIS-GIT/DynamixelSDK',
     author=authors,
